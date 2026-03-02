5e8b05216c31ec2077b95c6cba4e8fc4ae1a5dc7
duoprog4 <l530g592@home.ku.edu>
2026-03-02 00:55:32 -0600

    fixed console output formatting, implemented option to reinput code after error, fixed function logic

--- temp.c
+++ temp.c
@@ -1,4 +1,4 @@
-#include <stdio.h>t
+#include <stdio.h>
 float celsius_to_fahrenheit(float celsius) {
     return (celsius * 9 / 5) + 32;
 }
@@ -18,7 +18,7 @@ void categorize_temperature(float celsius) {
         if (celsius >= 0 && celsius < 10) {
             printf("cold, wear a jacket");
         }
-        if (celsius >= 10 && celsius < 20) {
+        if (celsius >= 10 && celsius < 25) {
             printf("comfortable, have fun!");
         }
         if (celsius >= 25 && celsius < 35) {
@@ -29,32 +29,58 @@ void categorize_temperature(float celsius) {
         }
 }
 int main() {
+    int loop = 1;
+
+    while (loop) {
+
     printf("enter temperature: ");
-    int temp;
-    scanf("%d", &temp);
-    printf("enter temperature type: (f for fahrenheit, c for celsius, k for kelvin): ");
-    char temp_type[1];
-    scanf("%s", temp_type);
-    printf("convert temp to : (f for fahrenheit, c for celsius, k for kelvin): ");
-    char temp_convert[1];
-    scanf("%s", temp_convert);
+    float temp;
+    if (scanf(" %f", &temp) != 1){
+        printf("invalid input, enter a number\n");
+        while (getchar() != '\n');
+        continue;
+    }
     float celsius;
-    if (temp_type[0] == 'c' && temp_convert[0] == 'f') {
-        printf("temperature in fahrenheit: %f", celsius_to_fahrenheit(temp));
+    printf("enter temperature type: (f for fahrenheit, c for celsius, k for kelvin): ");
+    char temp_type;
+    scanf(" %c", &temp_type);
+    if (temp_type == 'c') {
         celsius = temp;
-    } else if (temp_type[0] == 'f' && temp_convert[0] == 'c') {
-        printf("temperature in celsius: %f", fahrenheit_to_celsius(temp));
+    } else if (temp_type == 'f') {
         celsius = fahrenheit_to_celsius(temp);
-    } else if (temp_type[0] == 'c' && temp_convert[0] == 'k') {
-        printf("temperature in kelvin: %f", celsius_to_kelvin(temp));
-        celsius = temp;
-    } else if (temp_type[0] == 'k' && temp_convert[0] == 'c') {
-        printf("temperature in celsius: %f", kelvin_to_celsius(temp));
+    } else if (temp_type == 'k') {
         celsius = kelvin_to_celsius(temp);
+        if (temp < 0) {
+            printf("invalid input, kelvin cannot be negative\n");
+            while (getchar() != '\n');
+            continue;
+        }
     } else {
-        printf("invalid input");
-        return 0;
+        printf("not a valid temp unit\n");
+        while (getchar() != '\n');
+        continue;
+    }
+    printf("convert temp to : (f for fahrenheit, c for celsius, k for kelvin): ");
+    char temp_convert;
+    scanf(" %c", &temp_convert);
+    if (temp_type == temp_convert){
+        printf("invalid input, cant convert to same unit\n");
+        while (getchar() != '\n');
+        continue;
+    }
+    if (temp_convert == 'c') {
+        printf("temperature in celsius: %.2f\n", celsius);
+    } else if (temp_convert == 'f') {
+        printf("temperature in fahrenheit: %.2f\n", celsius_to_fahrenheit(celsius));
+    } else if (temp_convert == 'k') {
+        printf("temperature in kelvin: %.2f\n", celsius_to_kelvin(celsius));
+    } else {
+        printf("not a valid temp unit");
+        while (getchar() != '\n');
+        continue;
     }
     categorize_temperature(celsius);
-     return 0;
+    loop=0;
+}
+    return 0;
 }
